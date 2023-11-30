66ce1e11cef0114c214eca54311482aca78e2e34
Frostyflames12 <rizqyriadi02@gmail.com>
2023-11-30 15:02:08 +0700

    Added result popup
    Added popup when timer expires

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
+import { challenges } from "./util/Challenges.js";
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
+        {/* {challenges.map((challengeItem) => (
+          <TimerChallenge
+            key={challengeItem.level}
+            title={challengeItem.level}
+            targetTime={challengeItem.time}
+          />
+        ))} */}
         <TimerChallenge title="Easy" targetTime={1} />
         <TimerChallenge title="Not Easy" targetTime={5} />
         <TimerChallenge title="Getting tough" targetTime={10} />
---
+++ src/util/challenges.js
@@ -0,0 +1,19 @@
+
+export const challenges = [
+    {
+        level: 'Easy',
+        time: '1s'
+    },
+    {
+        level: 'Not Easy',
+        time: '5s'
+    },
+    {
+        level: 'Getting tough',
+        time: '10s'
+    },
+    {
+        level: 'Pros only',
+        time: '15s'
+    },
+]
